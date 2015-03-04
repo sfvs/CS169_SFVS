52ae504ddf0c8d71764eeb5ee1c57c5023310bfb
alan <alantly@berkeley.edu>
2015-03-03 22:12:15 -0400

    made application & health form tables, connected the tables

--- SFVS_Registration_System/db/migrate/20150304013541_create_users.rb
+++ SFVS_Registration_System/db/migrate/20150304013541_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+	  add_column :users, :username, :string
+	  add_column :users, :password, :string
       t.timestamps
     end
   end
