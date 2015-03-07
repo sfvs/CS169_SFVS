1b1e3b3df3e32e2f4e1e8828a36a11014fe766ab
brokenfingers <hyunsikk91@gmail.com>
2015-03-07 00:16:39 -0800

    login functionality and admin page functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,13 +3,13 @@ class HomeController < ApplicationController
 	def index
 		#just render index page
 		if user_signed_in?
+      if current_user.admin?
+        redirect_to admin_path
+        return
+      end
 			redirect_to user_path(current_user)
 		else
 			redirect_to new_user_session_path
-    else
-      if current_user.admin?
-        redirect_to admin_path
-      end
 		end
 	end
 
