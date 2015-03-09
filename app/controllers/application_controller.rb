658d46d68bf43c608d91005ed57d57ab5a6bc45c
brokenfingers <hyunsikk91@gmail.com>
2015-03-08 18:28:32 -0700

    fixed global login check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :require_login
- 
-  private
- 
-  def require_login
-    unless user_signed_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_user_session_path # halts request cycle
-    end
-  end
+  before_filter :authenticate_user!
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,10 @@
 class HomeController < ApplicationController
 
-  skip_before_filter :require_login, only: [:index]
-
   def index
-    if user_signed_in?
-      if current_user.admin?
-        redirect_to admin_users_path
-      else
-        redirect_to user_path(current_user)
-      end
+    if current_user.admin?
+      redirect_to admin_users_path
     else
-      redirect_to new_user_session_path
+      redirect_to user_path(current_user)
     end
   end
 
