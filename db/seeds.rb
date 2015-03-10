7dd9f49196792f63949748d247117896f63668a9
alan <alantly@berkeley.edu>
2015-03-10 14:42:29 -0300

    reverted to old attr_reader format, and using :without_protection in rspec and cucumber

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-def create_a_user(email, password, admin_status = false)
-	user1 = User.new({:email => email, :password => password})
-	user1.admin = admin_status
-	user1.save
+def create_a_user(email, password, admin_status = :user)
+	admin = :admin == admin_status
+	User.create({:email => email, :password => password, :admin => admin}, :without_protection => true)
 end
 
-create_a_user('admin@hostname.com', 'admin123', true)
+create_a_user('admin@hostname.com', 'admin123', :admin)
 create_a_user('user2@hostname.com', 'user1234')
 create_a_user('user3@hostname.com', 'user1234')
 create_a_user('user4@hostname.com', 'user1234')
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,8 +1,6 @@
 # login steps
 Given(/^the following users exist:$/) do |table|
   table.hashes.each do |user|
-    User.create(user)
+    User.create(user, :without_protection => true)
   end
 end
-
-
