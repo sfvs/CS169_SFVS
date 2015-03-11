cb046374c8617d5fc142cf8143d262538430c5e2
brokenfingers <hyunsikk91@gmail.com>
2015-03-10 21:58:43 -0700

    working on authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery
   before_filter :authenticate_user!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
 end
