c5250452a4bf293ec7aee5e8da710962a40c4655
brokenfingers <hyunsikk91@gmail.com>
2015-03-12 04:59:28 -0700

    Changing

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,4 +1,5 @@
 class Admin::AdminController < ApplicationController
+  # include UsersHelper # Perhaps need to create the same method in AdminHelper and include it?
 
   def index
     @users = User.find(:all, :conditions => ["admin = ?", false])
@@ -6,11 +7,13 @@ class Admin::AdminController < ApplicationController
   end
 
   def show
-    show_and_edit
+    @user = User.find_by_id(params[:id])
+    authorize current_user
   end
 
   def edit
-    show_and_edit
+    @user = User.find_by_id(params[:id])
+    authorize current_user
   end
 
   def create
@@ -19,15 +22,5 @@ class Admin::AdminController < ApplicationController
 
   def destroy
     authorize current_user
-  end
-
-  private
-  def show_and_edit
-    if User.exists?(params[:id])
-      @user = User.find(params[:id])
-      authorize current_user
-    else
-      redirect_to admin_root_path
-    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
 
   def show
-    if User.exists?(params[:id])
-      @user = User.find(params[:id])
-      authorize current_user, :is_regular_user?
-      authorize @user, :is_profile_owner?
-    else
+    @user = User.find_by_id(params[:id])
+    if @user.nil?
       redirect_to root_path
     end
+    authorize current_user, :is_regular_user?
+    authorize @user, :is_profile_owner?
   end
 end
