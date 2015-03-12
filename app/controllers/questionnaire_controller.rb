0a282e4a2cb5c61964e4dda800da9ef640f46899
kiwi0r0 <kiwicakii@gmail.com>
2015-03-12 03:45:42 -0300

    Clean up extra routes, some refactoring, connect to profile

---
+++ app/controllers/questionnaire_controller.rb
@@ -0,0 +1,36 @@
+class QuestionnaireController < ApplicationController
+	@@ROOT_QUESTION_ID = 1
+	def show
+
+		# @display is an array of hashes, containing question and its answers 
+		# as Rows in Table Answers. something like, 
+		# [{:question => "What's my name?", :answer => AnswersTable},...]
+		@display = Array.new {Hash.new}
+		@selected_a_id = []
+		@end_of_questionnaire = false	#set default value
+
+
+		current_qid = @@ROOT_QUESTION_ID	#base case, at root question
+		if params[:ans] != nil	#got an answer
+			a = Answers.find(params[:ans])
+			if a.leads_to == nil	#case1: a is the last answer
+				@end_of_questionnaire = true
+				@selected_a_id.push(a.id) 	#corner case: add the last selected answer into array
+				@form = a.id
+				current_qid = a.questionnaire_id
+			else	#case2: a is an intermidate answer
+				current_qid = a.leads_to
+			end
+		end
+
+		while current_qid != nil #populate @display with q/a pairs
+			q = Questionnaire.find(current_qid)
+			as = Answers.where(questionnaire_id: current_qid)
+			@display.unshift({:question => q.question, :answer => as})
+			if current_qid != 1  
+				@selected_a_id.push(Answers.get_answer_id(q.id, q.parent_id)) 
+			end
+			current_qid = q.parent_id
+		end
+	end
+end
