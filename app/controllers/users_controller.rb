365e4986183939aa917461d2077f7abbeb11cc4b
alan <alantly@berkeley.edu>
2015-03-13 03:08:54 -0300

    refactored display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,25 +3,24 @@ class UsersController < ApplicationController
   before_filter :require_valid_user
 
   def show
-    @user = User.find_by_id(params[:id])
+    @user = User.find(params[:id])
     @response = parse_questionnaire_response(params[:questionnaire_response])
   end
 
   def questionnaire
-    @@ROOT_QUESTION_ID = 1
-
     # @display is an array of hashes, containing question and its answers 
     # as Rows in Table Answers. something like, 
     # [{:question => "What's my name?", :answer => AnswersTable},...]
-    @display = Array.new {Hash.new}
-    @selected_a_id = []
+
+    @current_selected_answers = [] # to hilite selected answers
     @end_of_questionnaire = false	#set default value
-    current_qid = @@ROOT_QUESTION_ID	#base case, at root question
+    current_qid = Questionnaire.get_root_question_id
+
     if params[:ans] != nil	#got an answer
       a = Answers.find(params[:ans])
       if a.leads_to == nil	#case1: a is the last answer
         @end_of_questionnaire = true
-        @selected_a_id.push(a.id) #corner case: add the last selected answer into array
+        @current_selected_answers.push(a.id) #corner case: add the last selected answer into array
         @form = a.id
         current_qid = a.questionnaire_id
       else	#case2: a is an intermidate answer
@@ -29,43 +28,49 @@ class UsersController < ApplicationController
       end
     end
 
-    while current_qid != nil	#populate @display with q/a pairs
-      q = Questionnaire.find(current_qid)
-      as = Answers.where(questionnaire_id: current_qid)
-      @display.unshift({:question => q.question, :answer => as})
-      if current_qid != 1
-        @selected_a_id.push(Answers.get_answer_id(q.id, q.parent_id)) 
-      end
-      current_qid = q.parent_id
-    end
+    @display_questions = populate_display current_qid
+
   end
 
   private
   
   def require_valid_user
     #if used everywhere, we should put in application controller
-    authorize User.find_by_id(params[:id]), :is_profile_owner?
+    authorize User.find(params[:id]), :is_profile_owner?
     authorize current_user, :is_regular_user?
   end
 
+  # make an array of with q/a pairs for display, also edites current_select_answers
+  def populate_display(current_qid)
+    questions = Array.new {Hash.new}
+    while current_qid != nil
+      current_question = Questionnaire.find(current_qid)
+      associated_answer = Answers.get_answer_from_question current_question
+
+      # put question to front so it is in order from first to last question
+      questions.unshift({:question => current_question.question, :answer => associated_answer})
+      
+      if current_qid != Questionnaire.get_root_question_id #why? better way to detect nil on final
+        @current_selected_answers.push(Answers.get_answer_id(current_question)) 
+      end
+
+      current_qid = current_question.parent_id
+    end
+    questions
+  end
+
   def parse_questionnaire_response(answer_id)
-    if answer_id != nil
-      if answer_id == "3"
+    if answer_id != nil 
+      if answer_id == "2" #way not not hard code in answer?
         "Good choice"
-      elsif answer_id == "4"
+      elsif answer_id == "3"
         "Hmmmmm"
-      elsif answer_id == "5"
+      elsif answer_id == "4"
         "Ewwwwww"
-      elsif answer_id == "6"
+      elsif answer_id == "5"
         "I knew it!"
-      elsif answer_id == "7"
-        "Cool"
-      elsif answer_id == "8"
-        "MANGO HABANERO"
-      elsif answer_id == "9"
-        "So you like to be second."
       else
-        "" #default response is empty
+        "" #default response is empty, should be throwing an exception
       end
     end
   end
--- app/models/answers.rb
+++ app/models/answers.rb
@@ -2,7 +2,12 @@ class Answers < ActiveRecord::Base
   attr_accessible :ans, :questionnaire_id, :leads_to
   belongs_to :questionnaire
 
-  def Answers.get_answer_id(qid, pid)
-  	return Answers.where(questionnaire_id: pid, leads_to: qid)[0].id
+  def self.get_answer_id(question)
+  	return Answers.where(questionnaire_id: question.parent_id, leads_to: question.id)[0].id
   end
+
+  def self.get_answer_from_question q
+  	self.where(questionnaire_id: q.id)
+  end
+
 end
