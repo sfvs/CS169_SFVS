d3d6db2f3951d32fd6e71d7ec253825dd78c6d6e
alan <alantly@berkeley.edu>
2015-03-12 21:25:44 -0300

    refactor admin check

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,30 +1,38 @@
 class Admin::AdminController < ApplicationController
 
+
+  before_filter :require_admin
+
   def index
     @users = User.find(:all, :conditions => ["admin = ?", false])
-    authorize current_user, :is_admin?
+    #authorize current_user, :is_admin?
   end
 
   def show
     @user = User.find(params[:id])
-    authorize current_user, :is_admin?
+    #authorize current_user, :is_admin?
   end
 
   def edit
     @user = User.find(params[:id])
-    authorize current_user, :is_admin?
+    #authorize current_user, :is_admin?
   end
 
   def create
-    authorize current_user, :is_admin?
+    #authorize current_user, :is_admin?
   end
 
   def destroy
     @user = User.find(params[:id])
-    authorize current_user, :is_admin?
     @user.destroy
     flash[:notice] = "User #{@user.email} has been removed."
     redirect_to admin_root_path
+  end
+
+  private
+
+  def require_admin
+    authorize current_user, :is_admin?
   end
 
 end
