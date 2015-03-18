9622961410c0fa70bd088524b86d93206e82eec7
alan <alantly@berkeley.edu>
2015-03-18 00:16:51 -0300

    fixed up some steps, and made some tests for cucumber questionnaire. questionnaire could use some refactoring on how answers should be parsed, right now it is hard coded

--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -2,3 +2,30 @@
 Given /^PENDING/ do
   pending
 end
+
+Given(/^I login as "(.*)" and password "(.*)"$/) do |email, password|
+  step "I am on the \"login\" page"
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  click_button "Log in"
+end
+
+Given(/^the following users exist:$/) do |table|
+  table.hashes.each do |user|
+    User.create(user, :without_protection => true)
+  end
+end
+
+Given(/^the following questions exist:$/) do |table|
+  table.hashes.each do |item|
+    # take not that id does not get assigned
+    # it is there to help find the parsed status
+    Questionnaire.create(item)
+  end
+end
+
+Given(/^the following answers exist:$/) do |table|
+  table.hashes.each do |item|
+    Answers.create(item)
+  end
+end
--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,43 +1,17 @@
-# login steps
-Given(/^the following users exist:$/) do |table|
-  table.hashes.each do |user|
-    User.create(user, :without_protection => true)
-  end
-end 
-
+# user profile steps
 Then(/^I should see my "email" as "(.*)"$/) do |email|
-	fill_in "user_email", :with => email
-	fill_in "user_password", :with => "bear12345"
-	click_button "Log in"
-	assert page.has_content?("#{email}")
+  assert page.has_content?("#{email}")
 end 
 
 And(/^I should see my "company" as "(.*)"$/) do |company|
-	assert page.has_content?("#{company}")
+  assert page.has_content?("#{company}")
 end
 
 And(/^I should see my "status" as "(.*)"$/) do |status|
-	assert page.has_content?("#{status}")
+  assert page.has_content?("#{status}")
 end
 
-And(/^I can see "(.*)"$/) do |survey|
-	fill_in "user_email", :with => "johndoe@gmail.com"
-	fill_in "user_password", :with => "bear12345"
-	click_button "Log in"
-	should have_button "Take Survey"
-end 
-
-When(/^I do follow "(.*)"$/)  do |survey|
-	questions = [{:question => 'Which one came first?'},
-			 {:question => 'How do you like it?', :parent_id => 1},
-			 {:question => 'which part of chicken do you like?', :parent_id => 1}]
-
-    questions.each do |q|
-		Questionnaire.create!(q)
-	end
-	click_button("#{survey}")
-end 
-
-Then(/^I should see the "survey" page for "(.*)"$/) do |email|
-	assert page.has_content?("Questionnaire")
-end 
+# questionnaire steps
+Then(/^I should see "(.*)" selected$/) do |answer|
+  assert find_link(answer)['class'] == 'selected'
+end
