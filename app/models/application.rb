c579a44a80d4869cd18549c95de9493b975678a8
alan <alantly@berkeley.edu>
2015-03-27 02:47:22 -0300

    fixed up tests for commit before

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,12 +2,12 @@ class Application < ActiveRecord::Base
   attr_accessible :user, :year, :type, :content, :completed
 
   def self.get_application_types
-  	application_types = {}
-  	application_types[:vendor] = Answers.find_by_ans(:Vendor).id.to_s
-  	application_types[:donor] = Answers.find_by_ans(:Donor).id.to_s
-  	application_types[:restaurant_concessionaire] = Answers.find_by_ans(:Restaurant_Concessionaire).id.to_s
-  	application_types[:other] = Answers.find_by_ans(:Other).id.to_s
-  	application_types
+    {
+      :vendor => "1",
+      :donor => "2",
+      :restaurant_concessionaire => "3",
+      :other => "4"
+    }
   end
 
 end
