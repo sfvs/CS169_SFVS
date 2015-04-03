a1509e34d58f0007a926067eb544fbf3d33f998b
brokenfingers <hyunsikk91@gmail.com>
2015-04-02 23:28:21 -0700

    Removed method

--- app/models/form_question.rb
+++ app/models/form_question.rb
@@ -5,8 +5,4 @@ class FormQuestion < ActiveRecord::Base
   def get_form_question_types
   	[:checkbox, :textbox, :radio_button, :statement]
   end
-
-  def self.get_questions_for_form(form_id)
-    self.where(:form_id => form_id).order(order: :desc)
-  end
 end
