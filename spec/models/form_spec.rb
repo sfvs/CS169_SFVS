4160e2ff8bef821c93bc510d793de9f0e5fcb441
alan <alantly@berkeley.edu>
2015-04-04 00:08:49 -0300

    some more rspec tests

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Form do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "form functions" do
+    it "tells me the number of questions for in a form" do
+      num_of_questions = 3
+      form = make_form_with_questions num_of_questions
+      Form.stub(:find).with(form.id).and_return(form)
+
+      Form.number_of_questions(form.id).should == form.form_questions.count
+    end
+  end
 end
