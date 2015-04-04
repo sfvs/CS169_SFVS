40ddf351d0d056f6feed6693da44f853df6785c7
brokenfingers <hyunsikk91@gmail.com>
2015-04-03 21:04:52 -0700

    Rspec for form model

--- spec/models/form_spec.rb
+++ spec/models/form_spec.rb
@@ -5,9 +5,7 @@ describe Form do
     it "tells me the number of questions for in a form" do
       num_of_questions = 3
       form = make_form_with_questions num_of_questions
-      Form.stub(:find).with(form.id).and_return(form)
-
-      Form.number_of_questions(form.id).should == form.form_questions.count
+      form.number_of_questions.should == form.form_questions.count
     end
   end
 end
