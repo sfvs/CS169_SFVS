2d3cf4d34ce9785ad7883b37045a780435e4fccb
brokenfingers <hyunsikk91@gmail.com>
2015-04-13 00:45:14 -0700

    Application forms

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -5,5 +5,8 @@ class Admin::ApplicationsController < Admin::AdminController
     # Options:
     # 1. Get user with user_id then get the corresponding application
     # 2. Get the application using application id (:id)
+    @user = User.find(params[:user_id])
+    @application = Application.find(params[:id])
+    @app_forms = @application.application_type.forms
   end
 end
