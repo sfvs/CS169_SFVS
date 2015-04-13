1421d3d0eb290cc3842da98ddfb427a04600db66
brokenfingers <hyunsikk91@gmail.com>
2015-04-13 03:09:17 -0700

    Refactoring method

--- app/controllers/admin/application_form_controller.rb
+++ app/controllers/admin/application_form_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ApplicationFormController < Admin::AdminController
+
+  include ApplicationHelper
+
   before_filter :require_admin
 
   def show
@@ -7,21 +10,8 @@ class Admin::ApplicationFormController < Admin::AdminController
     # Note might need to write a method to get the form_contents
     @application = Application.find(params[:id])
     if @application.content.has_key?(params[:form_type])
-      @form_answer = get_answers_to_prefill_from(@application.content[params[:form_type]])
+      @form_answer = get_answers_to_prefill_from(@application.content[params[:form_type]], params[:form_type])
     end
     @list_of_questions = FormQuestion.get_questions_for_form(params[:form_type])
-  end
-
-  def get_answers_to_prefill_from(content)
-    number_of_questions = Form.where(form_name: params[:form_type])[0].number_of_questions
-    form_answer = Hash.new
-    cur_num_questions = 0
-    content.each do |key, value|
-      if cur_num_questions != number_of_questions && key != "completed"
-        form_answer[cur_num_questions.to_s] = value
-        cur_num_questions += 1
-      end
-    end
-    form_answer
   end
 end
--- app/controllers/form_question_controller.rb
+++ app/controllers/form_question_controller.rb
@@ -1,5 +1,7 @@
 class FormQuestionController < ApplicationController
   
+  include ApplicationHelper
+
   def show
     @form_type = params[:type]
     @list_of_questions = FormQuestion.get_questions_for_form(@form_type)
@@ -7,7 +9,7 @@ class FormQuestionController < ApplicationController
     if application
       @saved_form = application.content
       if @saved_form.has_key?(@form_type)
-        @form_answer = get_answers_to_prefill_from(@saved_form[@form_type])
+        @form_answer = get_answers_to_prefill_from(@saved_form[@form_type], @form_type)
       end
     end
   end
@@ -23,19 +25,6 @@ class FormQuestionController < ApplicationController
     elsif params[:commit] == 'Submit'
       submit
     end
-  end
-
-  def get_answers_to_prefill_from(content)
-    number_of_questions = Form.where(form_name: @form_type)[0].number_of_questions
-    form_answer = Hash.new
-    cur_num_questions = 0
-    content.each do |key, value|
-      if cur_num_questions != number_of_questions && key != "completed"
-        form_answer[cur_num_questions.to_s] = value
-        cur_num_questions += 1
-      end
-    end
-    form_answer
   end
 
   #filter that check if the form is filled correctly
