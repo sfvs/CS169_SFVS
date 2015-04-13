82745a03cac8b76f0718b5613684ba8b3cbd1720
brokenfingers <hyunsikk91@gmail.com>
2015-04-13 01:33:40 -0700

    Admin can now see user application forms

--- app/controllers/admin/application_form_controller.rb
+++ app/controllers/admin/application_form_controller.rb
@@ -7,10 +7,21 @@ class Admin::ApplicationFormController < Admin::AdminController
     # Note might need to write a method to get the form_contents
     @application = Application.find(params[:id])
     if @application.content.has_key?(params[:form_type])
-      @form_contents = @application.content[params[:form_type]]
-    else
-      @form_contents = FormQuestion.get_list_of_questions(params[:form_type])
+      @form_answer = get_answers_to_prefill_from(@application.content[params[:form_type]])
     end
-    logger.debug "Form contents: #{@form_contents}"
+    @list_of_questions = FormQuestion.get_questions_for_form(params[:form_type])
+  end
+
+  def get_answers_to_prefill_from(content)
+    number_of_questions = Form.where(form_name: params[:form_type])[0].number_of_questions
+    form_answer = Hash.new
+    cur_num_questions = 0
+    content.each do |key, value|
+      if cur_num_questions != number_of_questions && key != "completed"
+        form_answer[cur_num_questions.to_s] = value
+        cur_num_questions += 1
+      end
+    end
+    form_answer
   end
 end
