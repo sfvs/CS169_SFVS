1d9ffd5d74ca410fc4787290dba2b1e162eaf577
brokenfingers <hyunsikk91@gmail.com>
2015-04-13 23:40:01 -0700

    Added new columns to users and modified seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :contact_person, :company_name, :telephone
   attr_reader :admin
   # attr_accessible :title, :body
 
