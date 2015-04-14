8c2f1015e960d7374db8f3cd8fc944a1725d3f40
brokenfingers <hyunsikk91@gmail.com>
2015-04-13 22:26:55 -0700

    Adding Application Form Controller RSpec

--- spec/controllers/admin/application_form_controller_spec.rb
+++ spec/controllers/admin/application_form_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Admin::ApplicationFormController do
+  login :admin
 
+  describe "show action" do
+
+    before :each do
+      @user = make_a_member :user, :email => "rspecftw@hostname.com"
+    end
+
+    make_a_vendor_application_for_user
+
+    before :each do
+      @user.reload
+      @application = @user.applications[0]
+      @form_name = @application.application_type.forms[0].form_name
+    end
+
+  end
 end
