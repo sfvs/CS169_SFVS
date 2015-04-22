702474bd52601ee7fe41c14f9beebe6202eed89b
alan <alantly@berkeley.edu>
2015-04-22 02:08:53 -0300

    changed des from str to text

--- db/migrate/20150417194225_add_description_to_application_types.rb
+++ db/migrate/20150417194225_add_description_to_application_types.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToApplicationTypes < ActiveRecord::Migration
   def change
-  	add_column :application_types, :description, :string, null: false, default: ""
+  	add_column :application_types, :description, :text, null: false, default: ""
   end
 end
