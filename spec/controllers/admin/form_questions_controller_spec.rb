cf6020ff81aee070462e49ee52911b2669f3e932
kiwi0r0 <kiwicakii@gmail.com>
2015-04-25 00:56:51 -0300

    restore test

--- spec/controllers/admin/form_questions_controller_spec.rb
+++ spec/controllers/admin/form_questions_controller_spec.rb
@@ -43,4 +43,49 @@ describe Admin::FormQuestionsController do
       updated_form_q.question_type.should eq "statement"
     end
   end
+  describe "admin delete form question" do
+    it "should delete the form question" do
+      lambda do
+        delete 'destroy', :form_id => @form.id, :id => @form.form_questions[0].id
+      end.should change(FormQuestion, :count).by(-1)
+    end
+
+    it "should redirect to form content page after deleting form question" do
+      delete 'destroy', :form_id => @form.id, :id => @form.form_questions[0].id
+      response.should redirect_to '/admin/forms/' + @form.id.to_s + '/form_questions' 
+    end
+  end
+
+  describe "form create form question" do
+    it "should create a form question" do
+      lambda do
+        post 'create', :form_id => @form.id, :form_question => {}
+      end.should change(FormQuestion, :count).by(+1)
+    end
+
+    it "should redirect to form content page after creating form question" do
+      form = make_a_form
+      attribute = {
+        :form_question => {:question => "Rspec test question", :question_type => "checkbox"}
+      }
+      checkbox_answers = {"0"=>"True", "1"=>"False", "2"=>"", "3"=>""}
+      post 'create', :form_id => form.id, :form_question => attribute[:form_question], :check_answer => checkbox_answers
+      response.should redirect_to '/admin/forms/' + form.id.to_s + '/form_questions' 
+    end
+  end
+
+  describe "form sorting form question" do
+    it "returns http success" do
+      put 'sort', :form_id => @form.id, :order => {}
+      response.should be_success
+    end
+
+    it "should update the order of the form questions" do
+      order = {"0"=>{"id"=>"1", "position"=>"3"}, "1"=>{"id"=>"2", "position"=>"2"}, "2"=>{"id"=>"3", "position"=>"1"}}
+      put 'sort', :form_id => @form.id, :order => order
+      response.should be_success
+      form_q = FormQuestion.where(:id => 1).first
+      form_q.order.should eq 3
+    end
+  end
 end
