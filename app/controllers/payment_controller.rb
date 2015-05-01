b73f9b12168e50d53f918ff150ff56392143261c
Christopher Coale <ccoale@berkeley.edu>
2015-05-01 03:02:20 -0300

    refactored payment controller and stubbed actual call to paypal in test

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -7,66 +7,56 @@ class PaymentController < ActionController::Base
   # by PayPal from the notification hook
   skip_before_filter :verify_authenticity_token, :only => [:verify_payment]
 
-  def verify_payment
-    # lookup the application by invoice number/item number
-    item_number = params[:invoice]
-    if (item_number.empty?)
-      print("A user tried to make a payment with an empty invoice number. Canceling transaction.")
-      redirect_to root_path
-      return
-    end
-
-    application = Application.find_by_invoice_number!(item_number)
-    txn_id = params[:txn_id]
-    if (application.has_paid == true or application.payment_id == txn_id)
-      print("A user attempted to pay for an application that has already been paid for. Canceling transaction.")
-      redirect_to root_path
-      return
-    end
-
-    # verify amount_due == mc_gross (from paypal/post)
-    gross = params[:mc_gross].to_f
-    if (gross != application.amount_due)
-      print("A user attempted to forge a payment. Canceling transaction.")
-      redirect_to root_path
-      return
-    end
-
-    # we need to send a post request to PayPal with the txn_id to get the info
-    # and verify that the payment info is correct
-    print("RAW REQUEST: " + request.raw_post)
+  def get_paypal_response(query)
     paypal_url_cmd = "https://www.sandbox.paypal.com/cgi-bin/webscr"
-    req_body = "cmd=_notify-validate&" + request.raw_post
+    req_body = "cmd=_notify-validate&" + query
     url = URI.parse(paypal_url_cmd)
     req = Net::HTTP::Post.new(url.path) 
     req.body = req_body
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     result = http.request(req)
-    
-    # make sure paypal verified this payment info
-    if (result.body.to_s != "VERIFIED")
-      application.pay_status = Application::PAYSTATUS_DECLINED # rejected, please manually inspect
-      application.save!
-      print("PayPal could not verify the information provided by the user. Please manually inspect this payment.")
-      redirect_to root_path
-      return
-    end
-    
-    # update the application with amount paid and pay status
-    application.amount_paid = gross
-    application.has_paid = true
-    if (params[:payment_status] == "Completed")
-      application.pay_status = Application::PAYSTATUS_PAID
-    elsif (params[:payment_status] == "Pending")
-      application.pay_status = Application::PAYSTATUS_PENDING
-    else
-      application.pay_status = Application::PAYSTATUS_DECLINED
-    end
 
-    application.payment_id = txn_id
-    application.pay_receipt = params.to_s + "PayPal Response: " + result.body.to_s
+    return result.body.to_s
+  end
+
+  def update_application_with_response(application, response, gross)
+    payment_status = {"Complete" => Application::PAYSTATUS_PAID, "Pending" => Application::PAYSTATUS_PENDING}
+    print(response)
+    print(params.to_s)
+    if (response != "VERIFIED" or (params[:payment_status] != "Complete" and params[:payment_status] != "Pending") )
+      application.pay_status = Application::PAYSTATUS_DECLINED
+      print("PayPal could not verify the information provided by the user. Please manually inspect this payment.")
+    else
+      application.pay_status = payment_status[params[:payment_status]]
+      application.amount_paid = gross
+      application.has_paid = true
+      application.payment_id = params[:txn_id]
+      application.pay_receipt = params.to_s + "RESULT = " + response.to_s
+    end
     application.save!
+  end
+
+  def params_are_valid?(application)
+    item_number = params[:invoice]
+    txn_id = params[:txn_id]
+    gross = params[:mc_gross].to_f
+
+    (not item_number.empty?) and (application.has_paid != true) and (application.payment_id != txn_id) and (gross == application.amount_due)
+  end
+
+  def verify_payment
+    # lookup the application by invoice number/item number
+    application = Application.find_by_invoice_number!(params[:invoice])
+
+    if (not params_are_valid?(application))
+      print("invalid post request. Canceling transaction.")
+      print(params[:mc_gross])
+    else
+      print("RAW REQUEST: " + request.raw_post)
+      result = get_paypal_response(request.raw_post)
+      update_application_with_response(application, result, params[:mc_gross].to_f)
+    end
 
     redirect_to root_path
   end
--- spec/controllers/payment_controller_spec.rb
+++ spec/controllers/payment_controller_spec.rb
@@ -51,6 +51,7 @@ describe PaymentController do
     end
 
     it "should fail if paypal cannot verify params" do
+      PaymentController.any_instance.stub(:get_paypal_response).and_return("INVALID")
       app = FactoryGirl.create(:application)
       app.invoice_number = "abc123"
       app.amount_due = 500
@@ -68,6 +69,7 @@ describe PaymentController do
     end
 
     it "should succeed if paypal can verify params" do
+      PaymentController.any_instance.stub(:get_paypal_response).and_return("VERIFIED")
       app = FactoryGirl.create(:application)
       app.invoice_number = "14304493741"
       app.amount_due = 380.00
@@ -77,8 +79,9 @@ describe PaymentController do
 
       User.stub(:get_most_recent_application).and_return(app)
       Application.any_instance.stub(:all_forms_completed?).and_return(true)
-      raw_query = "mc_gross=380.00&invoice=14304493741&protection_eligibility=Ineligible&address_status=confirmed&payer_id=7RTK29EQXKAU6&tax=0.00&address_street=1+Main+St&payment_date=20%3A05%3A34+Apr+30%2C+2015+PDT&payment_status=Pending&charset=windows-1252&address_zip=95131&first_name=test&address_country_code=US&address_name=test+buyer&notify_version=3.8&custom=&payer_status=verified&address_country=United+States&address_city=San+Jose&quantity=1&verify_sign=A0W3PHNbCM.v5Sv7AarFHkovUSzRAzf4QEuJ8..JaPB8U7WSRbQG0rw1&payer_email=teamsfvs-buyer%40gmail.com&txn_id=4CE52036EV1277053&payment_type=instant&last_name=buyer&address_state=CA&receiver_email=sfvsteam-business%40gmail.com&pending_reason=unilateral&txn_type=web_accept&item_name=SFVS+Registration+Fee&mc_currency=USD&item_number=14304493741&residence_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_gross=380.00&shipping=0.00&ipn_track_id=a3639521b67b5"
-      raw_post(:verify_payment, Rack::Utils.parse_nested_query(raw_query), raw_query)
+      #raw_query = "mc_gross=380.00&invoice=14304493741&protection_eligibility=Ineligible&address_status=confirmed&payer_id=7RTK29EQXKAU6&tax=0.00&address_street=1+Main+St&payment_date=20%3A05%3A34+Apr+30%2C+2015+PDT&payment_status=Pending&charset=windows-1252&address_zip=95131&first_name=test&address_country_code=US&address_name=test+buyer&notify_version=3.8&custom=&payer_status=verified&address_country=United+States&address_city=San+Jose&quantity=1&verify_sign=A0W3PHNbCM.v5Sv7AarFHkovUSzRAzf4QEuJ8..JaPB8U7WSRbQG0rw1&payer_email=teamsfvs-buyer%40gmail.com&txn_id=4CE52036EV1277053&payment_type=instant&last_name=buyer&address_state=CA&receiver_email=sfvsteam-business%40gmail.com&pending_reason=unilateral&txn_type=web_accept&item_name=SFVS+Registration+Fee&mc_currency=USD&item_number=14304493741&residence_country=US&test_ipn=1&handling_amount=0.00&transaction_subject=&payment_gross=380.00&shipping=0.00&ipn_track_id=a3639521b67b5"
+      #raw_post(:verify_payment, Rack::Utils.parse_nested_query(raw_query), raw_query)
+      post :verify_payment, {:invoice => "14304493741", :mc_gross => "380.00", :txn_id => "abc123", :payment_status => "Pending"}
       app.reload
 
       expect(app.pay_status).to be(Application::PAYSTATUS_PENDING)
