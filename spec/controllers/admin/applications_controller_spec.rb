6a8964ea593a9bdc572d6fab52d2c606d4385550
brokenfingers <hyunsikk91@gmail.com>
2015-05-01 15:18:42 -0700

    Rspec tests

--- spec/controllers/admin/applications_controller_spec.rb
+++ spec/controllers/admin/applications_controller_spec.rb
@@ -36,5 +36,19 @@ describe Admin::ApplicationsController do
       response.should be_success
       assigns(:completed_forms).should == {@form_name => true}
     end
+
+    it "should aprove if the application is complete" do
+      @application.update_attribute(:completed, true)
+      post 'approve', :user_id => @user.id, :id => @application.id, :approve => true
+      @application.reload
+      @application.approved.should == true
+    end
+
+    it "should not aprove if the application is not complete" do
+      @application.update_attribute(:completed, false)
+      post 'approve', :user_id => @user.id, :id => @application.id, :approve => true
+      @application.reload
+      @application.approved.should == false
+    end
   end
 end
