7e96e42f8083455045415a2577d1498c22fbedaf
Christopher Coale <ccoale@berkeley.edu>
2015-05-01 00:50:37 -0300

    changed the mailer options back

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,6 @@ SFVSRegistrationSystem::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
