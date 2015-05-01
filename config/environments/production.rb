d5de8399178b6eaaeab0fc57bcfb106d47130381
Christopher Coale <ccoale@berkeley.edu>
2015-05-01 03:42:24 -0300

    restored email delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ SFVSRegistrationSystem::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.raise_delivery_errors = false
+  #config.action_mailer.raise_delivery_errors = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
