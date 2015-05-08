d6017fee291bf6e12880d341234818b0d980b369
Christopher Coale <ccoale@berkeley.edu>
2015-05-07 21:20:15 -0300

    Added pay by check feature

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -22,4 +22,21 @@ class Admin::ApplicationsController < Admin::AdminController
     end
     redirect_to admin_user_path(params[:user_id])
   end
+
+  def mark_paid
+    @application = Application.find(params[:id])
+    if !@application.completed? or @application.payment.has_paid
+      flash[:alert] = @application.completed? == false ? "The application must be completed by the user before it can be marked as paid." : "The user has already paid for this application."
+    else
+      @application.payment.has_paid = true
+      @application.payment.amount_paid = @application.payment.amount_due
+      @application.payment.pay_status = Payment::PAYSTATUS_CHECK
+      @application.payment.pay_receipt = "Manually marked as paid by administrator. Paid by check."
+      @application.payment.invoice_number = "CHECK" + Time.now.to_i.to_s
+      @application.payment.txn_id = @application.payment.invoice_number
+      @application.payment.save!
+      flash[:success] = "Successfully updated payment status."
+    end
+    redirect_to admin_user_path(params[:user_id])
+  end
 end
