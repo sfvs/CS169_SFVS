5e31d360c99c701a8e922a02188615905e1992d1
Christopher <ccoale@berkeley.edu>
2015-05-15 20:06:22 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,7 +70,7 @@ class UsersController < ApplicationController
   def get_paypal_url(payment)
     base_url = request.protocol + request.host
     values = {
-        business: "sfvsteam-business@gmail.com",
+        business: "treasurer@sfvs.org",
         cmd: "_xclick",
         upload: 1,
         return: base_url + "/payment_receipt",
